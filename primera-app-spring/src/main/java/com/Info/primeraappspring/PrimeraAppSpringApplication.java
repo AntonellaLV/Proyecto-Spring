9d2c876fee467fe6a24d93883465de9dcb2a2708
AntonellaLV <118840064+AntonellaLV@users.noreply.github.com>
2023-12-09 18:30:59 -0300

    Add Repo

---
+++ primera-app-spring/src/main/java/com/Info/primeraappspring/PrimeraAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.Info.primeraappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeraAppSpringApplication.class, args);
+	}
+
+}
